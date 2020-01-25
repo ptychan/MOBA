656453615c359ba96ddff0cfaeb9ce9138bb800a
Peter Chan <ptychan@hotmail.com>
2020-01-25 12:58:16 -0800

    Initial commit.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed;
+    
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.position += new Vector3(h, 0, v) * moveSpeed * Time.deltaTime;
+    }
+}
